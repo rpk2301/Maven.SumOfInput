cdf7518a5dfd4e774d96e333ff960a17203edea7
ricky <rpk2301@vt.edu>
2023-06-28 19:06:25 -0400

    I have finished the lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,36 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
+
+
+
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         int number = getInputNumber();
-        
-        int sum = sumOfNumbers(number);
-        
-        System.out.println(sum);
+
+
+        int sum2 = sumOfNumbers(number);
+       // int sum = gaussianSumOfNumbers(number);
+       // System.out.println("The sum of numbers using the gaussian method: " + sum);
+        System.out.println("The sum of numbers using the written method: " + sum2);
+
 
     }
-    
+
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    public static int sumOfNumbers(int n) {
+        int roll = 0;
+        for (int i = 1; i <= n; i++) {
+            roll = roll + i;
+        }
+        int time = (int) System.currentTimeMillis();
+        System.out.println("The current time using the written method is: " + time);
+        return roll;
     }
-    
-    
-    int getInputNumber() {
+
+
+    public static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -31,15 +41,38 @@ public class Main {
         scan.close();
         return num;
     }
-    
-    
-    
+
+
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    public static int gaussianSumOfNumbers(int n) {
+        int sum = 0;
+        int pairs = n / 2;
+
+        {
+            for (int i = 1; i <= pairs; i++) {
+                sum += (n + 1);
+            }
+            int time = (int) System.currentTimeMillis();
+            System.out.print("System time using the gaussian method: " + time);
+
+
+            return sum;
+        }
+
+
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
     // use "System.currentTimeMillis()" to ask the system what time it is.
+
+
+
+
+    //sum of numbers was faster than gaussian by several milliseconds
+
+
+
+
+
 }
